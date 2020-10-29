242de849fafed806d5c5c264172be62d143ff694
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-10-29 18:36:54 -0300

    update

---
+++ backend/src/config/server.js
@@ -0,0 +1,9 @@
+const port = 3003;
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const server = express();
+
+server.use(bodyParser.urlencoded({ extended: true }));
+
+server.listen(port, () => {console.log(`backend is running on port ${port}`)})
