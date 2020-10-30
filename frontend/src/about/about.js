a1bb41561dbdd3116e68a9f20ecd23d1abecf741
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-10-30 17:01:19 -0300

    update

--- frontend/src/about/about.js
+++ frontend/src/about/about.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import PageHeader from '../template/pageHeader';
 
 export default props => {
     return(
         <div>
-            <h1>Sobre</h1>
+            <PageHeader name="Sobre" small="nós" />
+            <h2>Nossa história</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta laudantium ut ipsum ad adipisci veritatis ducimus nulla earum, officia odio deleniti blanditiis eveniet illo unde nam. Cumque nobis dolorem quasi.</p>
         </div>
     )
 }
--- frontend/src/todo/todo.js
+++ frontend/src/todo/todo.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import PageHeader from '../template/pageHeader';
+import TodoForm from './todoForm';
+import TodoList from './todoList';
 
 export default class Todo extends Component {
     render() {
         return(
             <div>
-                <h1>ola</h1>
+                <PageHeader name="Tarefas" small="Cadastrar" />
+
+                <TodoForm />
+
+                <TodoList />
             </div>
         )
     }
