0d80891e81bc28f581e4d87de7411f5401913332
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-11-07 21:47:47 -0300

    update

--- frontend/src/todo/todoList.js
+++ frontend/src/todo/todoList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
 import IconButton from '../template/iconButton';
+import { markAsDone, markAsPending, remove } from './todoActions';
 
 const TodoList = props => {
     const renderRows = () => {
@@ -9,9 +11,9 @@ const TodoList = props => {
             return <tr key={todo._id}>
                 <td className={todo.done ? 'markAsDone' : ''}>{todo.description}</td>
                 <td>
-                    <IconButton style="success" icon="check" onClick={() => props.handleMarkAsDone(todo)} hide={todo.done} />
-                    <IconButton style="warning" icon="undo" onClick={() => props.handleMarkAsPending(todo)} hide={!todo.done} />
-                    <IconButton style="danger" icon="trash-o" onClick={() => props.handleRemove(todo)} hide={!todo.done}/>
+                    <IconButton style="success" icon="check" onClick={() => props.markAsDone(todo)} hide={todo.done} />
+                    <IconButton style="warning" icon="undo" onClick={() => props.markAsPending(todo)} hide={!todo.done} />
+                    <IconButton style="danger" icon="trash-o" onClick={() => props.remove(todo)} hide={!todo.done}/>
                 </td>
             </tr>;
         });
@@ -36,4 +38,6 @@ const mapStateToProps = state => ({
     list: state.todo.list
 });
 
-export default connect(mapStateToProps)(TodoList);
+const mapDispatchToProps = dispatch => bindActionCreators({ markAsDone, markAsPending, remove}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
